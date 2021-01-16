5f2d2fd1f414c0f053f6dd6816114d247bd3f473
jhoander <jhoander.torres@externos-cl.cencosud.com>
2021-01-16 20:29:56 -0300

    Se crean objetos a utilizar

---
+++ app/src/main/java/com/jhoander/therickandmorty/domain/model/Cartoon.kt
@@ -0,0 +1,5 @@
+package com.jhoander.therickandmorty.domain.model
+
+data class Cartoon(
+    val results : List<Character>
+)
---
+++ app/src/main/java/com/jhoander/therickandmorty/domain/model/Character.kt
@@ -0,0 +1,9 @@
+package com.jhoander.therickandmorty.domain.model
+
+data class Character(
+    val name: String?,
+    val image: String?,
+    val status: String?,
+    val species: String?,
+    val gender: String?
+)
